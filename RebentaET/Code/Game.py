084452b9fb04f76a2da4e8711e038e2996d4e572
Paulo Ferdin <paulinhomax1@hotmail.com>
2024-04-11 18:51:14 -0300

    Text Menu

--- RebentaET/Code/Game.py
+++ RebentaET/Code/Game.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from Code.Const import WIN_WIDTH, WIN_HEIGHT
+from Code.Menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
-        pass
+
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+
 
