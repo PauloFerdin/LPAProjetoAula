6e386145ebbb5be4a1fe3f64632e59315b3aed2f
Paulo Ferdin <paulinhomax1@hotmail.com>
2024-04-16 16:03:27 -0300

    Aula sobre criação e movimento de entidades

--- RebentaET/Code/Const.py
+++ RebentaET/Code/Const.py
@@ -1,2 +1,26 @@
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 128)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COOPERATIVE',
+               'EXIT')
+# W
 WIN_WIDTH = 1920
 WIN_HEIGHT = 1080
+
+#E
+ENTITY_SPEED = {'Level1Bg0': 0,
+                'Level1Bg1': 10,
+                'Level1Bg2': 11,
+                'Level1Bg3': 12,
+                'Level1Bg4': 13,
+                'Level1Bg5': 14,
+                'Level1Bg6': 15,
+                'Level1Bg7': 16,
+                'Level1Bg8': 17}
+
+
--- RebentaET/Code/Game.py
+++ RebentaET/Code/Game.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import sys
+
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from Code.Const import WIN_WIDTH, WIN_HEIGHT
+from Code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from Code.Level import Level
 from Code.Menu import Menu
 
 
@@ -13,10 +16,16 @@ class Game:
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self, ):
-
+    def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            else:
+                pygame.quit()
+                sys.exit()
 
 
